5f748e4c91beeec87468096e11f9bc31d2cb67ea
Milan21032008 <bambhaniyamilu2008@gmail.com>
2026-06-06 21:06:38 +0530

    Removed vercel.json to allow cron-job.org

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' }, // Google Auth Avatars
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' }, // Firebase Storage
+      { protocol: 'https', hostname: 'ui-avatars.com' }, // AI aur Default Avatars
+      { protocol: 'https', hostname: 'image.pollinations.ai' }, // Free AI Cover Photos
+      { protocol: 'https', hostname: 'images.unsplash.com' }, // Default Banners aur Stock Photos
     ],
   },
 };
